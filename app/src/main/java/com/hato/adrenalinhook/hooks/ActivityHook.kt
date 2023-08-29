f1921ad121e20013a04ee1c3e77b97e2e02e9ef2
Raúl Álvarez <raulwii2@gmail.com>
2023-08-29 14:12:11 +0200

    Working hooks

--- app/src/main/java/com/hato/adrenalinhook/hooks/ActivityHook.kt
+++ app/src/main/java/com/hato/adrenalinhook/hooks/ActivityHook.kt
@@ -3,7 +3,6 @@ package com.hato.adrenalinhook.hooks
 import android.content.Context
 import android.content.Intent
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XC_MethodReplacement
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
@@ -22,42 +21,38 @@ class ActivityHook : IXposedHookLoadPackage {
             "com.adrenalinagol.na.SplashActivity",
             lpparam?.classLoader
         ).declaredMethods) {
-            if (method.name.equals("showDNSDialog")) {
-                var params: MutableList<Any> = ArrayList()
-                params.addAll(method.parameterTypes.toList())
-                params.add(object : XC_MethodReplacement() {
-                    @Throws(Throwable::class)
-                    override fun replaceHookedMethod(param: MethodHookParam?): Any? {
-                        var context: Context = param?.thisObject as Context
-                        XposedBridge.log("Ejecutando función interceptada...")
-                        try {
-                            val mainActivityClassName: String = "com.adrenalinagol.na.MainActivity"
-                            var mainActivityClass: Class<*>? =
-                                context.classLoader.loadClass(mainActivityClassName)
-                            var intent: Intent = Intent(context, mainActivityClass)
-                            context.startActivity(intent)
-                            XposedBridge.log("Ejecutada correctamente!")
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                        }
-                        return null
-                    }
-                })
+            if (method.name.equals("showDNSDialog") || method.name.equals("showRootDialog")) {
 
                 XposedBridge.log("Intercentando metodo: $method")
 
-                XposedHelpers.findAndHookMethod("com.adrenalinagol.na.SplashActivity",
-                    lpparam?.classLoader, method.name, params.toTypedArray(), object : XC_MethodHook() {
-                        override fun beforeHookedMethod(param: MethodHookParam?) {
-                            super.beforeHookedMethod(param)
-                        }
+                XposedHelpers.findAndHookMethod(
+                    "com.adrenalinagol.na.SplashActivity",
+                    lpparam?.classLoader,
+                    method.name,
+                    object : XC_MethodReplacement() {
+                        @Throws(Throwable::class)
+                        override fun replaceHookedMethod(param: MethodHookParam): Any? {
+                            val context = param.thisObject as Context
 
-                        override fun afterHookedMethod(param: MethodHookParam?) {
-                            super.afterHookedMethod(param)
+                            try {
+                                val mainActivityClassName = "com.adrenalinagol.na.MainActivity"
+                                val mainActivityClass =
+                                    context.classLoader.loadClass(mainActivityClassName)
+
+                                val intent = Intent(context, mainActivityClass)
+
+                                context.startActivity(intent)
+                            } catch (e: ClassNotFoundException) {
+                                e.printStackTrace()
+                            }
+
+                            return null
                         }
-                    })
+                    }
+                )
                 hookedmethods++
             }
         }
+
     }
 }
--- app/src/main/java/com/hato/adrenalinhook/hooks/StreamDataHook.kt
+++ app/src/main/java/com/hato/adrenalinhook/hooks/StreamDataHook.kt
@@ -1,14 +1,10 @@
 package com.hato.adrenalinhook.hooks
 
-import android.content.Context
-import android.content.Intent
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XC_MethodReplacement
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
-import java.lang.reflect.Method
 
 class StreamDataHook : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
@@ -22,43 +18,40 @@ class StreamDataHook : IXposedHookLoadPackage {
             "com.adrenalinagol.na.PlayerActivity",
             lpparam?.classLoader
         ).declaredMethods) {
-            if(method.name.equals("setFullScreenListener")){
-                var params: MutableList<Any> = ArrayList()
-                params.addAll(method.parameterTypes.toList())
-                params.add(object : XC_MethodReplacement() {
-                    @Throws(Throwable::class)
-                    override fun replaceHookedMethod(param: MethodHookParam?): Any? {
-                        var thisObject = param?.thisObject;
-                        var getStreamDataMethod = thisObject?.javaClass?.getDeclaredMethod("getStreamData")
-                        if (getStreamDataMethod != null) {
+            if (method.name.equals("setFullScreenListener")) {
+                XposedHelpers.findAndHookMethod(
+                    "com.adrenalinagol.na.PlayerActivity",
+                    lpparam?.classLoader,
+                    method.name,
+                    object : XC_MethodReplacement() {
+                        @Throws(Throwable::class)
+                        override fun replaceHookedMethod(param: MethodHookParam): Any? {
+                            val thisObject = param.thisObject
+
+                            val getStreamDataMethod =
+                                thisObject.javaClass.getDeclaredMethod("getStreamData")
                             getStreamDataMethod.isAccessible = true
-                            var streamDataObject = getStreamDataMethod.invoke(thisObject)
-                            if(streamDataObject != null){
-                                var toStringMethod = getStreamDataMethod.javaClass.getMethod("toString")
+                            val streamDataObject = getStreamDataMethod.invoke(thisObject)
+
+                            if (streamDataObject != null) {
+                                val toStringMethod =
+                                    streamDataObject.javaClass.getMethod("toString")
                                 toStringMethod.isAccessible = true
-                                var toStringResult = toStringMethod.invoke(streamDataObject)
-                                if(toStringResult is String){
-                                    val streamDataToString = toStringResult as String
-                                    XposedBridge.log(streamDataToString)
+                                val toStringResult = toStringMethod.invoke(streamDataObject)
+
+                                if (toStringResult is String) {
+                                    XposedBridge.log(toStringResult)
                                 }
                             }
+
+                            return XposedBridge.invokeOriginalMethod(
+                                param.method,
+                                param.thisObject,
+                                param.args
+                            )
                         }
-                        return null
                     }
-                })
-
-                XposedBridge.log("Intercentando metodo: $method")
-
-                XposedHelpers.findAndHookMethod("com.adrenalinagol.na.PlayerActivity",
-                    lpparam?.classLoader, method.name, params.toTypedArray(), object : XC_MethodHook() {
-                        override fun beforeHookedMethod(param: MethodHookParam?) {
-                            super.beforeHookedMethod(param)
-                        }
-
-                        override fun afterHookedMethod(param: MethodHookParam?) {
-                            super.afterHookedMethod(param)
-                        }
-                    })
+                )
                 hookedmethods++
             }
         }
